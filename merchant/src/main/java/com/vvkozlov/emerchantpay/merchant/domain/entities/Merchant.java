f53a102b123c01e0ed2c426887cfbe2f9dd8c390
Messerschmitt <messer262a2@gmail.com>
2023-08-28 01:42:19 +0300

    Rename table merchant->merchants in merchant service for consistency

--- merchant/src/main/java/com/vvkozlov/emerchantpay/merchant/domain/entities/Merchant.java
+++ merchant/src/main/java/com/vvkozlov/emerchantpay/merchant/domain/entities/Merchant.java
@@ -8,7 +8,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "merchant")
+@Table(name = "merchants")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
---
+++ merchant/src/main/resources/db/migration/V4__Rename_Table.sql
@@ -0,0 +1,1 @@
+ALTER TABLE merchant RENAME TO merchants;
