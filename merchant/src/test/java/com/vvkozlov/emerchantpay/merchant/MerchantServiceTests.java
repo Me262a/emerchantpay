4406583104707d6db21f8087a620d1b08278f898
Messerschmitt <messer262a2@gmail.com>
2023-08-28 20:14:18 +0300

    Clean up tests, WIP

--- merchant/src/test/java/com/vvkozlov/emerchantpay/merchant/MerchantServiceTests.java
+++ merchant/src/test/java/com/vvkozlov/emerchantpay/merchant/MerchantServiceTests.java
@@ -2,7 +2,6 @@ package com.vvkozlov.emerchantpay.merchant;
 
 import com.vvkozlov.emerchantpay.merchant.infra.repository.MerchantRepository;
 import com.vvkozlov.emerchantpay.merchant.service.MerchantService;
-import com.vvkozlov.emerchantpay.merchant.service.contract.OAuthServerAdminClient;
 import com.vvkozlov.emerchantpay.merchant.service.model.MerchantEditDTO;
 import com.vvkozlov.emerchantpay.merchant.service.model.MerchantViewDTO;
 import com.vvkozlov.emerchantpay.merchant.service.util.OperationResult;
@@ -12,89 +11,63 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 import java.util.Optional;
-import java.util.UUID;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.Mockito.anyString;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "spring.flyway.enabled=false"
+})
 class MerchantServiceTests {
-
-    @Mock
-    private MerchantRepository merchantRepository;
-
-    @Mock
-    private OAuthServerAdminClient oAuthServerAdminClient;
-
     @InjectMocks
     private MerchantService merchantService;
 
+    @Mock
+    private MerchantRepository merchantRepository;
+/*Remove warnings
+    @Mock
+    private OAuthServerAdminClient oAuthServerAdminClient;
+
+    @Mock
+    private MessageBrokerProducer mbProducer;
+
+    @Mock
+    private TransactionMsClient transactionMsClient;*/
+
+
     @BeforeEach
-    void setUp() {
+    public void setUp() {
         MockitoAnnotations.openMocks(this);
     }
-/*
-    @Test
-    void testGetMerchant_ExistingId() {
-        String authId = UUID.randomUUID().toString();
-        Merchant mockMerchant = new Merchant();
-        Optional<Merchant> mockOptional = mock(Optional.class);
-        when(mockOptional.isPresent()).thenReturn(true);
-        when(mockOptional.get()).thenReturn(mockMerchant);
-        when(merchantRepository.findByAuthId(authId)).thenReturn(mockOptional);
-
-        OperationResult<MerchantViewDTO> result = merchantService.getMerchant(authId);
-
-        assertThat(result.isSuccess()).isTrue();
-        verify(merchantRepository, times(1)).findByAuthId(authId);
-    }*/
 
     @Test
-    void testGetMerchant_NonExistingId() {
-        String authId = UUID.randomUUID().toString();
-        when(merchantRepository.findByAuthId(authId)).thenReturn(Optional.empty());
+    public void getMerchant_WithInvalidId_ReturnsFailure() {
+        // Setup
+        when(merchantRepository.findByAuthId(anyString())).thenReturn(Optional.empty());
 
-        OperationResult<MerchantViewDTO> result = merchantService.getMerchant(authId);
+        // Execute
+        OperationResult<MerchantViewDTO> result = merchantService.getMerchant("invalidAuthId");
 
-        assertThat(result.isSuccess()).isFalse();
-        assertThat(result.getErrors()).contains("Merchant not found");
+        // Assert
+        assertFalse(result.isSuccess());
     }
-/*
-    @Test
-    void testUpdateMerchant_ExistingId() {
-        String authId = UUID.randomUUID().toString();
-        Merchant mockMerchant = new Merchant();
-        MerchantEditDTO editDTO = new MerchantEditDTO();
-        when(merchantRepository.findByAuthId(authId)).thenReturn(Optional.of(mockMerchant));
-
-        OperationResult<MerchantViewDTO> result = merchantService.updateMerchant(authId, editDTO);
-
-        assertThat(result.isSuccess()).isTrue();
-        verify(merchantRepository, times(1)).save(any(Merchant.class));
-    }*/
 
     @Test
-    void testUpdateMerchant_NonExistingId() {
-        String authId = UUID.randomUUID().toString();
-        MerchantEditDTO editDTO = new MerchantEditDTO();
-        when(merchantRepository.findByAuthId(authId)).thenReturn(Optional.empty());
+    public void updateMerchant_WithInvalidId_ReturnsFailure() {
+        // Setup
+        when(merchantRepository.findByAuthId(anyString())).thenReturn(Optional.empty());
 
-        OperationResult<MerchantViewDTO> result = merchantService.updateMerchant(authId, editDTO);
+        MerchantEditDTO dto = new MerchantEditDTO();
 
-        assertThat(result.isSuccess()).isFalse();
-        assertThat(result.getErrors()).contains("Merchant not found");
+        // Execute
+        OperationResult<MerchantViewDTO> result = merchantService.updateMerchant("invalidAuthId", dto);
+
+        // Assert
+        assertFalse(result.isSuccess());
     }
-/*
-    @Test
-    void testImportMerchantsFromCsv() throws Exception {
-        when(oAuthServerAdminClient.addUser(anyString(), anyList())).thenReturn(OperationResult.success("Some AuthId"));
-
-        OperationResult<List<MerchantViewDTO>> result = merchantService.importMerchantsFromCsv();
-
-        assertThat(result.isSuccess()).isTrue();
-        assertThat(Collections.singletonList(result.getResult())).isNotEmpty();
-        verify(merchantRepository, atLeastOnce()).save(any(Merchant.class));
-    }*/
 }
