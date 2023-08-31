9c8b9d8f96abbcdfc38737bc2ac0a40cc450d922
Messerschmitt <messer262a2@gmail.com>
2023-08-31 19:26:00 +0300

    Minor UX enhancement to transaction details - Add copy UUID icon

--- web-ui/src/components/transaction/TransactionDetails.jsx
+++ web-ui/src/components/transaction/TransactionDetails.jsx
@@ -1,62 +1,76 @@
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router";
-import { Link } from "react-router-dom";
-import { getTransaction } from '../../modules/transactions';
+import {useEffect, useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {useParams} from "react-router";
+import {Link} from "react-router-dom";
+import {getTransaction} from '../../modules/transactions';
 import TransactionViewModel from "../../domain/transaction/TransactionViewModel";
+import {FaCopy} from "react-icons/fa";
+import {toast} from "react-toastify";
 
 const TransactionDetails = () => {
-  const { transactionId } = useParams();
-  const dispatch = useDispatch();
-  const transactionData = useSelector(state => state.transactions.currentTransactionDetails); // Assuming you have a slice for the current transaction in your state
-  const [transaction, setTransaction] = useState(undefined);
+    const {transactionId} = useParams();
+    const dispatch = useDispatch();
+    const transactionData = useSelector(state => state.transactions.currentTransactionDetails); // Assuming you have a slice for the current transaction in your state
+    const [transaction, setTransaction] = useState(undefined);
 
-  useEffect(() => {
-    dispatch(getTransaction(transactionId));
-  }, [dispatch, transactionId]);
+    useEffect(() => {
+        dispatch(getTransaction(transactionId));
+    }, [dispatch, transactionId]);
 
-  useEffect(() => {
-    if (transactionData) {
-      const transactionObj = new TransactionViewModel(
-          transactionData.uuid,
-          transactionData.belongsTo,
-          transactionData.status,
-          transactionData.transactionType,
-          transactionData.referenceId,
-          transactionData.customerEmail,
-          transactionData.customerPhone,
-          transactionData.amount
-      );
-      setTransaction(transactionObj);
-    }
-  }, [transactionData]);
+    useEffect(() => {
+        if (transactionData) {
+            const transactionObj = new TransactionViewModel(
+                transactionData.uuid,
+                transactionData.belongsTo,
+                transactionData.status,
+                transactionData.transactionType,
+                transactionData.referenceId,
+                transactionData.customerEmail,
+                transactionData.customerPhone,
+                transactionData.amount
+            );
+            setTransaction(transactionObj);
+        }
+    }, [transactionData]);
 
-  return transaction ? (
-      <div className="row">
-        <div className="col-sm-12">
-          <h3>Details for Transaction ID {transaction.uuid}</h3>
-          <hr/>
-          <h5>Belongs To</h5>
-          <p className="lead">{transaction.belongsTo}</p>
-          <h5>Status</h5>
-          <p className="lead">{transaction.status}</p>
-          <h5>Type</h5>
-          <p className="lead">{transaction.transactionType}</p>
-          <h5>Reference ID</h5>
-          <p className="lead">{transaction.referenceId ? transaction.referenceId : '-'}</p>
-          <h5>Customer Email</h5>
-          <p className="lead">{transaction.customerEmail ? transaction.customerEmail : '-'}</p>
-          <h5>Customer Phone</h5>
-          <p className="lead">{transaction.customerPhone ? transaction.customerPhone : '-'}</p>
-          <h5>Amount</h5>
-          <p className="lead">{transaction.amount ?? '-'}</p>
-          <hr/>
-          <p>
-            <Link to="/">&laquo; back to list</Link>
-          </p>
+    return transaction ? (
+        <div className="row">
+            <div className="col-sm-12">
+                <h3>
+                    Details for Transaction ID
+                    <span style={{color: 'darkblue', marginLeft: '5px'}}>
+                        {transaction.uuid}
+                    </span>
+                    <FaCopy
+                        onClick={() => navigator.clipboard.writeText(transaction.uuid).then(() => {
+                            toast.info('UUID copied to clipboard');
+                        }).catch(err => {
+                            toast.error('Failed to copy text: ', err);
+                        })}
+                        style={{marginLeft: "10px", marginBottom: "10px", cursor: "pointer", verticalAlign: "middle"}}/>
+                </h3>
+                <hr/>
+                <h5>Belongs To</h5>
+                <p className="lead">{transaction.belongsTo}</p>
+                <h5>Status</h5>
+                <p className="lead">{transaction.status}</p>
+                <h5>Type</h5>
+                <p className="lead">{transaction.transactionType}</p>
+                <h5>Reference ID</h5>
+                <p className="lead">{transaction.referenceId ? transaction.referenceId : '-'}</p>
+                <h5>Customer Email</h5>
+                <p className="lead">{transaction.customerEmail ? transaction.customerEmail : '-'}</p>
+                <h5>Customer Phone</h5>
+                <p className="lead">{transaction.customerPhone ? transaction.customerPhone : '-'}</p>
+                <h5>Amount</h5>
+                <p className="lead">{transaction.amount ?? '-'}</p>
+                <hr/>
+                <p>
+                    <Link to="/">&laquo; back to list</Link>
+                </p>
+            </div>
         </div>
-      </div>
-  ) : null;
+    ) : null;
 }
 
 export default TransactionDetails;
